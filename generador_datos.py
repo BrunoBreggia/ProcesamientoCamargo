5f005c2e1c112a3338ac30d94e1e96e87bbaab98
Bruno Breggia <breggiabruno@gmail.com>
2023-03-01 19:23:38 -0300

    Agrego parametros a la funcion generador de señales.

--- generador_datos.py
+++ generador_datos.py
@@ -4,21 +4,36 @@ from matlabStruct import create_matlabStruct
 from Senial import Senial
 
 # variables de archivo:
-n_file = 14  # 0
-filename = 'sujetos/' + file_list[n_file]
-toe = 'rtoe'
-angle = 'rhip_addu'
-ciclo = 'swing'  # puede ser 'full', 'swing', 'stance', 'nods'
-norm = True
+# n_file = 0  # 14
+# filename = 'sujetos/' + file_list[n_file]
+# toe = 'rtoe'
+# angle = 'rhip_addu'
+# ciclo = 'swing'  # puede ser 'full', 'swing', 'stance', 'nods'
+# norm = True
 
 
-def obtener_senial():
+def obtener_senial(n_file, toe, angle, ciclo, norm):
     """
     Devuelve un objeto señal listo para ser pasada como entrada a la red neuronal mine
     que evaluara la informacion mutua entre la altura del pie y la señal de apertura angular.
     Este archivo debera ser modificado para generar señales diferentes. La funcion debra ser
     llamada desde otro script y no recibe ningun parametro.
+
+    :param
+    n_file [int]
+        Numero del archivo de la simulacion (de 0 a 21)
+    toe [str]: "rtoe", "ltoe"
+        Cual pie analizar
+    angle [str]: "rankle" (entre otros)
+        Cual angulo analizar (los nombres estan en el archivo de iterables)
+    ciclo [str]: "full", "swing", "stance", "nods"
+        Tipo de ciclo de marcha a considerar en la señal
+    norm [bool]
+        Bandera para normalizar la señal. La normalización es estadística,
+        y se realiza en función del ciclo full. Luego se recorta, si es solicitado.
     """
+
+    filename = 'sujetos/' + file_list[n_file]
 
     # pre-procesamiento:
     Sujeto = create_matlabStruct(filename)
